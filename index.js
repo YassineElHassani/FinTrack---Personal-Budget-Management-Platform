7aa942eca63a1d41db8d91484fe3c0d52fbed742
Yassine El hassani <yassine.ds.404@gmail.com>
2025-09-23 14:28:05 +0100

    Installing 'dotenv' & Adding the  structer to the database and server file

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const sequelize = require("./config/database");
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Simple route
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("Hello, Express + Sequelize + MySQL!");
+});
+
+// Connect to DB
+sequelize.authenticate().then(() => {
+    console.log("✅ Database connected...");
+}).catch((err) => {
+    console.error("❌ Error connecting to DB:", err);
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
