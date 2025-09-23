cfdf09b8ffd68df650444d74018df49929601e5a
Yassine El hassani <yassine.ds.404@gmail.com>
2025-09-23 11:59:56 +0100

    Database Creation Querys

---
+++ db_querys.sql
@@ -0,0 +1,46 @@
+create database fin_track;
+
+create table users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+create table budgets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    name VARCHAR(255) NOT NULL, -- exp*. "September Budget"
+    total_amount DECIMAL(10,2) NOT NULL,
+    month_year VARCHAR(7), -- exp*. "2025-09"
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+create table categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    category_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+create table transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    category_id INT,
+    amount DECIMAL(10,2) NOT NULL,
+    type ENUM('income','expense') NOT NULL,
+    transaction_date DATE NOT NULL,
+    description TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
+);
+
+create table savings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    goal_name VARCHAR(255),
+    goal_amount DECIMAL(10,2) NOT NULL,
+    saved_amount DECIMAL(10,2) DEFAULT 0,
+    target_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
