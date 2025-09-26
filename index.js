eeeed1ee4dbb23ac3d875e5024e92d3445d7378c
Yassine El hassani <yassine.ds.404@gmail.com>
2025-09-26 09:15:54 +0100

    Installing EJS template engine & setuping the routes for navigation

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const sequelize = require("./config/database");
+const path = require('path');
+const routes = require("./routes/Routes");
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Setup EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.use("/", routes);
 
 // Simple route
 app.get("/", (req, res) => {
